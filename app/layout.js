a1db4b4c0f91b305fcd9e8c3c8c928931151e348
ALNAFI <alnafi.eng@gmail.com>
2026-03-29 22:38:22 +0600

    Prozen nextjs base

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body className="body-color">
         <Providers>{children}</Providers>
       </body>
